36019b65cefa1d72dd1482ba76a3a09d0ee0ecd3
Nestor <jodar.infmtic@gmail.com>
2018-08-12 03:06:14 +0200

    cambios travis

--- config/database.php
+++ config/database.php
@@ -56,9 +56,9 @@ return [
         'testing' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'homestead_test'),
-            'username'  => env('DB_USERNAME', 'homestead'),
-            'password'  => env('DB_PASSWORD', 'secret'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
